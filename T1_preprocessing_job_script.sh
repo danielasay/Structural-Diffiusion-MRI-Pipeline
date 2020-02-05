45a7a756703e167b24e82b28f450b71cd99d5182
danielasay <59574167+danielasay@users.noreply.github.com>
2020-02-05 11:29:05 -0700

    Update T1_preprocessing_job_script.sh

--- T1_preprocessing_job_script.sh
+++ T1_preprocessing_job_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --time=24:00:00   # walltime
+#SBATCH --time=00:15:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=16G  # memory per CPU core
