99187da7e9df55aa2ab5daf981dbab958f88d96b
danielasay <59574167+danielasay@users.noreply.github.com>
2020-02-19 13:56:25 -0700

    Update c4d_extract_script.sh

--- c4d_extract_script.sh
+++ c4d_extract_script.sh
@@ -7,7 +7,7 @@ for i in sub*; do
 
 niftiCount=`c4d ~/compute/EDSD/deriv/pDWI/${i}/dwi.nii.gz -info-full | grep -Ewo "dim\[4\] = [0-9]{1,3}" | cut -d = -f2`
 bvalCount=`cat ~/compute/EDSD/deriv/pDWI/${i}/dwi.bval | wc -w`
-bvecCount=`head ~/compute/EDSD/deriv/pDWI/${i}/dwi.bvec | wc -w`
+bvecCount=`head -n 1 ~/compute/EDSD/deriv/pDWI/${i}/dwi.bvec | wc -w`
 
 echo "$i, $bvalCount, $bvecCount, $niftiCount" >> b_values.csv
 done
