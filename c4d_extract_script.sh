41703b452f61296b762ed0ea5bf8fb1349a8eb17
danielasay <59574167+danielasay@users.noreply.github.com>
2020-02-19 13:48:44 -0700

    Update c4d_extract_script.sh

--- c4d_extract_script.sh
+++ c4d_extract_script.sh
@@ -5,9 +5,9 @@ cd ~/compute/EDSD/deriv/pDWI
 
 for i in sub*; do
 
-bvalCount=`c4d ~/compute/EDSD/deriv/pDWI/${i}/dwi.nii.gz -info-full | grep -Ewo "dim\[4\] = [0-9]{1,3}" | cut -d = -f2`
-bvecCount=`c4d ~/compute/EDSD/deriv/pDWI/${i}/dwi.nii.gz -info-full | grep -Ewo "dim\[3\] = [0-9]{1,3}" | cut -d = -f2`
-totalvol=`c4d ~/compute/EDSD/deriv/pDWI/${i}/dwi.nii.gz -info-full | grep -Ewo "dim\[2\] = [0-9]{1,3}" | cut -d = -f2`
+niftiCount=`c4d ~/compute/EDSD/deriv/pDWI/${i}/dwi.nii.gz -info-full | grep -Ewo "dim\[4\] = [0-9]{1,3}" | cut -d = -f2`
+bvalCount=`cat ~/compute/EDSD/deriv/pDWI/${i}/dwi.bval | wc -w`
+bvecCount=`head ~/compute/EDSD/deriv/pDWI/${i}/dwi.bvec | wc -w`
 
-echo "$i, $bvalCount, $bvecCount, $totalvol" >> b_values.csv
+echo "$i, $bvalCount, $bvecCount, $niftiCount" >> b_values.csv
 done
