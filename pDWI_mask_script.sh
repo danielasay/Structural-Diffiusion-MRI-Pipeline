246fb75838f5994ca61085dad8883c92f71bea9e
danielasay <59574167+danielasay@users.noreply.github.com>
2020-02-18 22:27:15 -0700

    Add files via upload

---
+++ pDWI_mask_script.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#SBATCH --time=00:30:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=16G  # memory per CPU core
+
+#LOAD Variables
+
+
+pWDI_path=~/compute/EDSD/deriv/pDWI/${1}
+
+
+# Make pDWI Directories and Symlink Files
+
+
+
+mkdir -p ~/compute/EDSD/deriv/pDWI/${1}
+
+cd ~/compute/EDSD/deriv/pDWI/${1}
+
+ln -s ~/compute/EDSD/${1}/dwi/${1}_dwi.nii.gz dwi.nii.gz
+ln -s ~/compute/EDSD/${1}/dwi/${1}_dwi.bval dwi.bval
+ln -s ~/compute/EDSD/${1}/dwi/${1}_dwi.bvec dwi.bvec
+
+
+
+
+# Extract B0 Image
+
+cd ${pWDI_path}
+
+fslroi dwi.nii.gz dwi_b0.nii.gz 0 1
+
+# Create Brain Mask
+
+cd ${pWDI_path}
+
+bet dwi_b0.nii.gz dwi_b0 -f 0.1 -g 0 -m -n
+
+
+
+
+
+
