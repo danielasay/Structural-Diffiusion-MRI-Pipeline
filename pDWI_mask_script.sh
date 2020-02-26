a48cce4dc06a5a57d8768b40228e341c77837911
danielasay <59574167+danielasay@users.noreply.github.com>
2020-02-26 09:03:39 -0700

    Update pDWI_mask_script.sh

--- pDWI_mask_script.sh
+++ pDWI_mask_script.sh
@@ -5,10 +5,7 @@
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=16G  # memory per CPU core
 
-#LOAD Variables
-
-
-pWDI_path=~/compute/EDSD/deriv/pDWI/${1}
+pDWI_path=~/compute/EDSD/deriv/pDWI/${1}
 
 
 # Make pDWI Directories and Symlink Files
@@ -17,7 +14,7 @@ pWDI_path=~/compute/EDSD/deriv/pDWI/${1}
 
 mkdir -p ~/compute/EDSD/deriv/pDWI/${1}
 
-cd ~/compute/EDSD/deriv/pDWI/${1}
+cd ${pDWI_path}
 
 ln -s ~/compute/EDSD/${1}/dwi/${1}_dwi.nii.gz dwi.nii.gz
 ln -s ~/compute/EDSD/${1}/dwi/${1}_dwi.bval dwi.bval
@@ -28,13 +25,13 @@ ln -s ~/compute/EDSD/${1}/dwi/${1}_dwi.bvec dwi.bvec
 
 # Extract B0 Image
 
-cd ${pWDI_path}
+cd ${pDWI_path}
 
 fslroi dwi.nii.gz dwi_b0.nii.gz 0 1
 
 # Create Brain Mask
 
-cd ${pWDI_path}
+cd ${pDWI_path}
 
 bet dwi_b0.nii.gz dwi_b0 -f 0.3 -g 0 -m -n
 
@@ -43,14 +40,14 @@ bet dwi_b0.nii.gz dwi_b0 -f 0.3 -g 0 -m -n
 cd ${pDWI_path}
 
 c3d \
-dwi_mask.nii.gz \
+dwi_b0_mask.nii.gz \
 -erode 1 1x1x1vox \
 -o dwi_mask_cleaned.nii.gz
 
 
 # Create Tree File
 
-tree fslhome/asayd2/compute/EDSD/deriv/pDWI > ~/pWDI.txt
+tree fslhome/asayd2/compute/EDSD/deriv/pDWI > ~/pDWI.txt
 
 
 
